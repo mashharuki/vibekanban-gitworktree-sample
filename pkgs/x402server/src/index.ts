1b2ab734be3f773d5d94f8f9c97d027d1d13799a
mashharuki <avp.104.106.107@ae.auone-net.jp>
2026-02-23 23:04:15 +0900

    fix

--- pkgs/x402server/src/index.ts
+++ pkgs/x402server/src/index.ts
@@ -1,9 +1,3 @@
 import app from "./app";
-import { createApp } from "./app-factory";
-
-export { createRoutes } from "./route";
-export { client, createResourceServer, resolvePaymentOptions } from "./utils/config";
-
-export { createApp };
 
 export default app;
